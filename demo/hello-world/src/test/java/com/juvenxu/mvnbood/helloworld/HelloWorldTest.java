bd54ff2c32be1b7521fdc31fde3b2675efab4114
youhh <youhh@diandainfo.com>
2018-06-04 14:48:19 +0800

    hello-worl

---
+++ demo/hello-world/src/test/java/com/juvenxu/mvnbood/helloworld/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.juvenxu.mvnbook.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import com.juvenxu.mvnbook.helloworld.HelloWorld;
+
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.print("Test===============");
+        String result = helloWorld.sayHello();
+
+        assertEquals( "Hello Maven", result );
+    }
+}
