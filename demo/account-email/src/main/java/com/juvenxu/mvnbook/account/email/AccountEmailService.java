1b759c4d4afefa1e137b5bb8779df9e2722460ed
youhh <youhh@diandainfo.com>
2018-07-02 11:04:37 +0800

    添加accout-email

---
+++ demo/account-email/src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailService.java
@@ -0,0 +1,6 @@
+package com.juvenxu.mvnbook.account.email;
+
+public interface AccountEmailService {
+	
+    void sendMail( String to, String subject, String htmlText ) throws AccountEmailException;
+}
